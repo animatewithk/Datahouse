82bad5166580f73f862234c635700c42d53c7b94
animatewithk <168434285+animatewithk@users.noreply.github.com>
2024-05-10 05:50:30 +0000

    Navbar logic 1

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let stri = ''
+let bio = ''
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -5,11 +7,26 @@ const path = require('path')
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true}))
+
 app.get("/", (req, res)=>{
-	res.render('index')
+	res.render('index', {
+		bio: bio,
+	})
 })
+
 app.post("/", (req, res)=>{
-	res.send('hi')
+	//res.send('loading...')
+	stri = req.body.options.join(', ')
+ //Assigning the value of biography
+	if(stri == 'Albert Einstein'){
+	 bio = 'Albert Einstein was a German-born theoret'
+ }else{
+	 bio = 'information not found yet'
+ }
+
+	res.redirect('/')
 })
 
 app.listen(3000, ()=>{
